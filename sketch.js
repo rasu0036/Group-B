4c14df6346ed01a25ca2ca1d2472fbdc750055db
Kate0334 <kelu0334@uni.sydney.edu.au>
2024-11-02 11:32:00 +0800

    change some details
    added an array and for loop for big circles

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 let colors = [];
+let bigCircles = [];
   
 function setup() {
     createCanvas(windowWidth, windowHeight);
     colorMode(HSB);
   
- //color array  
- colors = [
+  //color array  
+  colors = [
     color('#242062'),//1 a
     color('#0C63AD'),//2 b
     color('#0E99A2'),//3 c
@@ -23,57 +24,67 @@ function setup() {
     color('#0E99A2'),//15 c
     color('#FDCE23')//16 d
    ];
+
+
+  //radius is scaled with canvas
+  let r = min(width, height) * 0.35; 
+
+  //change at 11.2, set an array to store the big circles maybe more readable
+  bigCircles = [
+    //x larger, more to the right; y larger, more to the bottom
+    //scale factors for positions are set one by one roughly instead of using a loop and formulas, 
+    //if they are incorrect or inconvenient, tell me in the group chat
+    new Circle(0.1, 0.05, r, colors[0]),//1
+    new Circle(0.11, 0.43, r, colors[1]),//2
+    new Circle(0.05, 0.8, r, colors[2]),//3
+    new Circle(0.35, 0, r, colors[3]),//4
+    new Circle(0.3, 0.38, r, colors[4]),//5
+    new Circle(0.24, 0.75, r, colors[5]),//6
+    new Circle(0.17, 1.1, r, colors[6]),//7
+    new Circle(0.5, 0.26, r, colors[7]),//8
+    new Circle(0.45, 0.68, r, colors[8]),//9
+    new Circle(0.40, 1.05, r, colors[9]),//10
+    new Circle(0.7, 0.24, r, colors[10]),//11
+    new Circle(0.68, 0.61, r, colors[11]),//12
+    new Circle(0.63, 0.98, r, colors[12]),//13
+    new Circle(0.89, 0.08, r, colors[13]),//14
+    new Circle(0.9, 0.57, r, colors[14]),//15
+    new Circle(0.87, 1, r, colors[15])//16
+    ];
   }
 
 //set up the class of the big circle
 class Circle {
-    //x and y scale based on the width and height
-    //thus the patterns can change with the size of the window 
-    constructor(xScale, yScale, r, color) {
-        this.xScale = xScale; 
-        this.yScale = yScale; 
-        this.r = r;
-        this.color = color;
+  //x and y scale based on the width and height
+  //thus the patterns can change with the size of the window 
+  constructor(xScale, yScale, r, color) {
+    this.xScale = xScale; 
+    this.yScale = yScale; 
+    this.r = r;
+    this.color = color;
     }
   
-    draw() {
-        fill(this.color);
-        noStroke();
+  draw() {
+    fill(this.color);
+    noStroke();
     //x and y actual position are width*scale factors
-    //scale factors are set manually, if they are incorrect or inconvenient, tell me in the group chat
-        let x = width * this.xScale;
-        let y = height * this.yScale;
-        ellipse(x, y, this.r);
+    let x = width * this.xScale;
+    let y = height * this.yScale;
+    ellipse(x, y, this.r);
     }
   }
   
 
 function draw() {
-    
-    background(255);
-    //radius is scaled with canvas
-    let r = min(width, height) * 0.35; 
+  background(255);
 
-    //draw the circles 
-    //x larger, more to the right; y larger, more to the bottom
-    new Circle(0.1, 0.05, r, colors[0]).draw();//1
-    new Circle(0.11, 0.43, r, colors[1]).draw();//2
-    new Circle(0.05, 0.8, r, colors[2]).draw();//3
-    new Circle(0.35, 0, r, colors[3]).draw();//4
-    new Circle(0.3, 0.38, r, colors[4]).draw();//5
-    new Circle(0.24, 0.75, r, colors[5]).draw();//6
-    new Circle(0.17, 1.1, r, colors[6]).draw();//7
-    new Circle(0.5, 0.26, r, colors[7]).draw();//8
-    new Circle(0.45, 0.68, r, colors[8]).draw();//9
-    new Circle(0.40, 1.05, r, colors[9]).draw();//10
-    new Circle(0.69, 0.24, r, colors[10]).draw();//11
-    new Circle(0.68, 0.61, r, colors[11]).draw();//12
-    new Circle(0.63, 0.98, r, colors[12]).draw();//13
-    new Circle(0.89, 0.08, r, colors[13]).draw();//14
-    new Circle(0.9, 0.57, r, colors[14]).draw();//15
-    new Circle(0.87, 1, r, colors[15]).draw();//16
+  //draw the big circles
+  for(let circle of bigCircles){
+    circle.draw();
+    }
+
   }
   
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
   }
